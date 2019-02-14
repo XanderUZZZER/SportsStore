925a7a0ba0efbf3fef0c15311c4da56827ab26d2
XanderUZZZER <xanderuzzzer@gmail.com>
2019-02-14 17:53:42 +0200

    Page 316

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            Product p = (Product)obj;
+            return ((Name == p.Name) && (Description == p.Description) && (Price == p.Price) && (Category == p.Category));
+        }
     }
 }
